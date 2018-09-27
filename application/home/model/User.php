eb47c7c6032ac2e6a664ef014107ef994bfa6a6f
tinyxm <511973588@qq.com>
2018-09-27 14:40:40 +0800

    新建控制器

---
+++ application/home/model/User.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace app\home\model;
+
+use think\Model;
+
+class User extends Model
+{
+    // 设置当前模型对应的完整数据表名称
+    protected $table = 'xm_user';
+
+    //自定义初始化
+    protected function initialize()
+    {
+        //需要调用`Model`的`initialize`方法
+        parent::initialize();
+        //TODO:自定义的初始化
+    }
+
+}
